d92df83647b63f5060b289e35f5a151ba3e49390
ndt2101 <69622101+ndt2101@users.noreply.github.com>
2021-10-18 01:24:47 +0700

    tablayout (chưa fix bottom navigation view)

---
+++ app/src/main/java/com/tuan2101/ezimarket/HomeFragment.kt
@@ -0,0 +1,27 @@
+package com.tuan2101.ezimarket
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import com.tuan2101.ezimarket.databinding.FragmentHomeBinding
+
+
+class HomeFragment : Fragment() {
+
+    lateinit var binding: FragmentHomeBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+//        (requireActivity() as AppCompatActivity).setSupportActionBar(binding.toolbar)
+        return binding.root
+    }
+
+
+}
--- app/src/main/java/com/tuan2101/ezimarket/MainActivity.kt
+++ app/src/main/java/com/tuan2101/ezimarket/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.tuan2101.ezimarket
 
+import android.content.res.ColorStateList
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.Window
+import android.view.WindowManager
+import androidx.core.widget.ImageViewCompat
+import androidx.databinding.DataBindingUtil
+import com.tuan2101.ezimarket.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        theme.applyStyle(R.style.Theme_HomeFragment, true)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding =  DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        // set color for icon of floating action button
+        ImageViewCompat.setImageTintList(
+            binding.cart,
+            ColorStateList.valueOf(resources.getColor(R.color.custom))
+        )
+
     }
+
+
 }
